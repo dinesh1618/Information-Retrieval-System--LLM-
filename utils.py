83e9ad4677273c682ef84a4a59203c1d07fd68e3
dinesh yakkanti <dineshyakkanti@gamil.com>
2023-12-15 09:43:21 +0530

    working..

--- utils.py
+++ utils.py
@@ -1,28 +1,47 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from PyPDF2 import PdfReader
 from ensure import ensure_annotations
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import GooglePalmEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 from langchain.vectorstores import FAISS
 from langchain.llms import GooglePalm
 
 load_dotenv()
 os.environ['GOOGLE_API_KEY']=os.getenv('GOOGLE_API_KEY')
 
-@ensure_annotations
-def PDFLoader(doc_path: Path) -> list:
-    doc_path = Path(doc_path)
-    loader = PyPDFLoader(doc_path)
-    return loader.load()
 
-@ensure_annotations
-def TextSplitter(list_docs: list, chunk_size=800, chunk_overlap=50) -> list:
-    text_spltter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    return text_spltter.split_documents()
+def get_pdf_text(pdf_docs):
+    pdf_reader = PdfReader(pdf_docs)
+    num_pages = len(pdf_reader.pages)
+    text = ""
+    for page_num in range(num_pages):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
 
-@ensure_annotations
-def VectorStore(list_splitdocs: list):
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+
+def get_vector_store(text_chunks):
     embeddings = GooglePalmEmbeddings()
-    return FAISS.from_documents(documents=list_splitdocs, embedding=embeddings)
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return vector_store
+
+
+
+def get_conversational_chain(vector_store):
+    llm=GooglePalm()
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
